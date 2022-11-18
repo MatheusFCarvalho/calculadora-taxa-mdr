542bc2c0a9c7135309a9bb2ef7149d337dc3f822
Matheus Florentino de Carvalho <matheus.fcarvalho2001@gmail.com>
2022-11-18 10:05:32 -0300

    fix: R$ in results

--- src/components/results/results.jsx
+++ src/components/results/results.jsx
@@ -5,13 +5,7 @@ function Results({ results }) {
     const centsValues = Object.values(results)
     const daysArray = ['Amanhã:', 'Em 15 dias:', 'Em 30 dias:', 'Em 90 dias:']
 
-    const format = (number) => (number / 100).toFixed(2)
-
-    // let dayOne = resultArray[0] / 100
-    // let weekTwo = resultArray[1] / 100
-    // let oneMonth = resultArray[2] / 100
-    // let threeMonths = resultArray[3] / 100
-
+    const format = (number) => 'R$' + (number / 100).toFixed(2)
 
     return (
         <Container>
@@ -20,10 +14,6 @@ function Results({ results }) {
                 <div className='sublinhado' />
             </div>
             {daysArray.map((day, i) => <p>{day} <b>{format(centsValues[i])}</b></p>)}
-            {/* <p>Amanhã: <strong>R${dayOne.toFixed(2)}</strong> </p>
-            <p>Em 15 dias: <strong>R${weekTwo.toFixed(2)}</strong> </p>
-            <p>Em 30 dias: <strong>R${oneMonth.toFixed(2)}</strong> </p>
-            <p>Em 90 dias: <strong>R${threeMonths.toFixed(2)}</strong> </p> */}
         </Container>
     )
 }
